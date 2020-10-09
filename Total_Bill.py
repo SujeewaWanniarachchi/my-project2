70f5e61ed41a8cb87042e7ed76071a59062334a9
Jayaweera <asjayaweera1996@gmail.com>
2020-10-09 06:59:10 +0530

    Minor variable change in Total_Bill program

--- Total_Bill.py
+++ Total_Bill.py
@@ -1,13 +1,13 @@
 
-Units=int(input("Enter Units:"))
-if Units<=50:
-     total=Units*5
-elif Units<=150:
-    total=(50*5)+((Units-50)*7)
-elif Units<=200:
-    total=50*5+100*7+((Units-150)*10)
+units=int(input("Enter Units:"))
+if units<=50:
+     total=units*5
+elif units<=150:
+    total=(50*5)+((units-50)*7)
+elif units<=200:
+    total=50*5+100*7+((units-150)*10)
 else:
-    total=50*5+100*7+100*10+((Units-200)*15)
+    total=50*5+100*7+100*10+((units-200)*15)
 
 print ("Total Bill:","Rs",total+(total*0.2))    
     
